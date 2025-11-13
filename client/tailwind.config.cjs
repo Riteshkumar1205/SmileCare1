6b1dd75bd0f35f7671a7120c72be2a19c5ec227f
Ritesh Kumar <glbitm2025@gmail.com>
2025-11-13 15:05:23 +0530

    Create tailwind.config.cjs

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#e0f2fe",
+          100: "#bae6fd",
+          500: "#0ea5e9",
+          700: "#0369a1"
+        }
+      }
+    }
+  },
+  plugins: [],
+};
